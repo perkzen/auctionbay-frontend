6a1c028b0aa9d3ce18878c494f977f0a453a14c0
perkzen <perko.domen@gmail.com>
2024-02-05 22:45:10 +0100

    fix: build

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,35 +1,30 @@
-import NextAuth, {User} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { User } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-
-export const {
-    handlers: {GET, POST},
-    auth,
-    signIn,
-    signOut,
-} = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: {label: "Email", type: "text"},
-                password: {label: "Password", type: "password"},
-            },
-            async authorize(credentials, req): Promise<User | null> {
-                // login user
-                return {id: "", name: "J Smith", email: ""};
-            },
-        }),
-    ],
-    callbacks: {
-        async jwt(params) {
-            // refresh token
-            return params;
-
-        },
-        async session({token, session}) {
-            // set session
-            return session;
-        },
-    }
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req): Promise<User | null> {
+        // login user
+        return { id: '', name: 'J Smith', email: '' };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt(params) {
+      // refresh token
+      return params;
+    },
+    async session({ token, session }) {
+      // set session
+      return session;
+    },
+  },
 });
+
+export { handler as GET, handler as POST };
