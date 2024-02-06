bad8656e4eb4ac9ea13a874bc67fddf8bcadbfbc
perkzen <perko.domen@gmail.com>
2024-02-06 22:17:24 +0100

    fix: margin

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import ForgotPasswordForm from '@/components/pages/forgot-password/forgot-passwo
 const ForgotPassword = () => {
   return (
     <div className={'m-auto'}>
-      <div className={'mb-16 flex flex-col gap-2 text-center'}>
+      <div className={'mb-8 flex flex-col gap-2 text-center'}>
         <h1 className={'text-3.5xl font-bold'}>Forgot password?</h1>
         <p className={'font-light'}>
           No worries, we will send you reset instructions
