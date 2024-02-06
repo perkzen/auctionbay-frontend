5f6fd5c8cca36e6a50699c4b26c7efeb6da71e8e
perkzen <perko.domen@gmail.com>
2024-02-06 16:44:05 +0100

    feat: working middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,19 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/protected'],
 };
+
+export default withAuth(
+  async (_req, _res) => {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized({ token }) {
+        return !!token;
+      },
+    },
+  }
+);
