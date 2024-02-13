1d83f05c9d5a014d00e2100f2cfe8a3b0dc6c5cc
perkzen <perko.domen@gmail.com>
2024-02-13 14:03:09 +0100

    fix: tab navigations

--- src/components/ui/link-tab.tsx
+++ src/components/ui/link-tab.tsx
@@ -13,7 +13,7 @@ const LinkTab = ({ children, href, isActive }: LinkTabProps) => {
       variant={isActive ? 'secondary' : 'ghost'}
       href={href}
       className={
-        'flex h-full w-fit items-center justify-center whitespace-nowrap rounded-4xl p-2 font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-secondary-100 data-[state=active]:text-white data-[state=active]:shadow-sm sm:p-4'
+        'flex h-full w-fit items-center justify-center whitespace-nowrap rounded-4xl p-2 font-medium transition-all sm:p-4'
       }
     >
       {children}
