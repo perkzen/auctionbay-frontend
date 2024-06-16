0d2261f98054c89c3b59156da2b8ada0de90c300
perkzen <perko.domen@gmail.com>
2024-06-16 18:29:19 +0200

    fix: file validation

--- src/libs/validators/create-auction-validator.ts
+++ src/libs/validators/create-auction-validator.ts
@@ -9,13 +9,16 @@ export const CreateAuctionValidator = z.object({
   startingPrice: numberString(),
   endDate: z.date(),
   fileList: z
-    .custom<FileList>()
-    .refine((data) => data.length > 0, {
+    .instanceof(FileList)
+    .refine((files) => files.length > 0, {
       message: 'Image is required',
     })
-    .refine((data) => data[0].size <= MAX_FILE_SIZE_BYTES, {
-      message: `Image size must be less than ${MAX_FILE_SIZE_MB}MB`,
-    }),
+    .refine(
+      (files) => Array.from(files).every((file) => file.size <= MAX_FILE_SIZE_BYTES),
+      {
+        message: `Image size must be less than ${MAX_FILE_SIZE_MB}MB`,
+      }
+    ),
 });
 
 export type CreateAuctionData = z.infer<typeof CreateAuctionValidator>;
--- src/libs/validators/update-profile-picture-validator.ts
+++ src/libs/validators/update-profile-picture-validator.ts
@@ -4,11 +4,14 @@ import { MAX_FILE_SIZE_BYTES, MAX_FILE_SIZE_MB } from '@/libs/constants';
 
 export const UpdateProfilePictureValidator = z.object({
   fileList: z
-    .custom<FileList>()
-    .refine((data) => data.length > 0, {
+    .instanceof(FileList)
+    .refine((files) => files.length > 0, {
       message: 'Image is required',
     })
-    .refine((data) => data[0].size <= MAX_FILE_SIZE_BYTES, {
-      message: `Image size must be less than ${MAX_FILE_SIZE_MB}MB`,
-    }),
+    .refine(
+      (files) => Array.from(files).every((file) => file.size <= MAX_FILE_SIZE_BYTES),
+      {
+        message: `Image size must be less than ${MAX_FILE_SIZE_MB}MB`,
+      }
+    ),
 });
