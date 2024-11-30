1fd04d6653f8929ca832559454997b8ee39a5a61
perkzen <perko.domen@gmail.com>
2024-11-30 20:52:21 +0100

    feat: Add hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { cn } from '@/libs/utils';
 import Providers from '@/components/providers/providers';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +20,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(inter.className, 'flex min-h-screen flex-col bg-base')}
-      >
+      <Script id="hotjar">
+        {`
+          (function (h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function () {
+                // eslint-disable-next-line prefer-rest-params
+                (h.hj.q = h.hj.q || []).push(arguments);
+              };
+            h._hjSettings = { hjid: ${5226377}, hjsv: 6 };
+            a = o.getElementsByTagName("head")[0];
+            r = o.createElement("script");
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+        `}
+      </Script>
+      <body className={cn(inter.className, 'flex min-h-screen flex-col bg-base')}>
         <Providers>{children}</Providers>
       </body>
     </html>
